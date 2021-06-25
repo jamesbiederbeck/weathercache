78b381a7987050e944ab8518e94547d56d3926d1
James Biederbeck <james@jamesbiederbeck.com>
2021-06-25 14:33:28 -0700

    Mock temperature endpoint

--- api.py
+++ api.py
@@ -3,4 +3,14 @@ import flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/temperature', methods=['GET'])
+def return_temperature():
+    """Returns json structure of current temperature like:
+        {"query_time":
+        <timestamp>, "temperature": <temperature> }
+    """
+    
+    data = {"query_time":1624654669, "temperature":88.14}
+    return json.dumps(data)
+
 app.run()
