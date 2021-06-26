c04c87ba2bd03039d187bf1a97784c56483d5b5f
James Biederbeck <james@jamesbiederbeck.com>
2021-06-25 19:04:38 -0700

    Make DB.py more pythonic

--- db.py
+++ db.py
@@ -1,10 +1,33 @@
 import sqlite3
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 
-con = sqlite3.connect('temperature_cache.db')
-cur = con.cursor()
+def get_db():
+    if 'db' not in g:
+        g.db=sqlite3.connect(
+            current_app.config['DATABASE']
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory =sqlite3.Row
+    return g.db
 
-# Create table
-cur.execute('''CREATE TABLE temperatures
-                (query_time datetime, temperature real)''')
-con.commit()
-con.close()
+def close_db(e=None):
+    db = g.pop('db',None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+    
+    with current_app.open_resource('schema.sql') as f:
+        db.executesript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Drop tables if they exist and create new tables"""
+    init_db()
+    click.echo("Initialized the database")
+
