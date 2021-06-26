c5488625ffc78138ab62fe47a1d914adc2f3dcf6
James Biederbeck <james@jamesbiederbeck.com>
2021-06-25 17:56:16 -0700

    have /temperature fetch current weather from openweather api

--- api.py
+++ api.py
@@ -12,10 +12,23 @@ def return_temperature():
         {"query_time":
         <timestamp>, "temperature": <temperature> }
     """
-    
-    data = {"query_time":1624654669, "temperature":88.14}
+    temperature = fetch_current_weather()
+    data = {"query_time":1624654669, "temperature":temperature}
     return json.dumps(data)
 
+def fetch_current_weather(city="portland"):
+    """Returns temperature
+    """
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    payload = {
+        'q':city, #this is the city parameter
+        'appid':openweatherkey,
+        'units':"imperial"
+    }
+    r = requests.get(url, params=payload)
+    temperature = r.json()['main']['temp']
+    return temperature
+    
 if __name__ == "__main__":
     app.run()
 
