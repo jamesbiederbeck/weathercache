d8f2169d27e446805d4e0dad553d389ada5c48f6
James Biederbeck <james@jamesbiederbeck.com>
2021-06-28 07:02:17 -0700

    Improve readability and fix typo in function name

--- weathercache/db.py
+++ weathercache/db.py
@@ -1,6 +1,8 @@
 import sqlite3
+
 import click
-from flask import current_app, g
+from flask import current_app
+from flask import g
 from flask.cli import with_appcontext
 
 def get_db():
@@ -20,17 +22,21 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
-    
-    with current_app.open_resource('schema.sql') as f:
-        db.executesript(f.read().decode('utf8'))
 
-@click.command('init-db')
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command("init-db")
 @with_appcontext
 def init_db_command():
     """Drop tables if they exist and create new tables"""
     init_db()
-    click.echo("Initialized the database")
+    click.echo("Initialized the database.")
+
 
 def init_app(app):
+    """Register database functions with the Flask app.
+    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
