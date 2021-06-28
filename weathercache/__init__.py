1bd8b53eee0ece210cf8a908732bf05fcb05b133
James Biederbeck <james@jamesbiederbeck.com>
2021-06-28 06:36:09 -0700

    Move flask application creation to factory

--- weathercache/__init__.py
+++ weathercache/__init__.py
@@ -4,18 +4,20 @@ import requests
 from .secrets import openweatherkey
 from datetime import datetime
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-
-@app.route('/temperature', methods=['GET'])
-def return_temperature():
-    """Returns json structure of current temperature like:
-        {"query_time":
-        <timestamp>, "temperature": <temperature> }
-    """
-    temperature = fetch_current_weather()
-    data = {"query_time":str(datetime.utcnow()) , "temperature":temperature}
-    return json.dumps(data)
+def create_app():
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    
+    @app.route('/temperature', methods=['GET'])
+    def return_temperature():
+        """Returns json structure of current temperature like:
+            {"query_time":
+            <timestamp>, "temperature": <temperature> }
+        """
+        temperature = fetch_current_weather()
+        data = {"query_time":str(datetime.utcnow()) , "temperature":temperature}
+        return json.dumps(data)
+    return app
 
 def fetch_current_weather(city="portland"):
     """Returns temperature
@@ -29,7 +31,3 @@ def fetch_current_weather(city="portland"):
     r = requests.get(url, params=payload)
     temperature = r.json()['main']['temp']
     return temperature
-    
-if __name__ == "__main__":
-    app.run()
-
